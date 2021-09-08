64ee601c3c3e3dae12906d8f6fc37f7728be9db6
kostyamospan <kostya04042001@gmail.com>
2021-09-08 18:01:15 +0300

    cherry-picked old overwritten commits
    abi and addresses
    
    c3d49e72e9b181e53f10eddcc09e17738f333089 cherry-pick merge conflict fix
    
    c3d49e72e9b181e53f10eddcc09e17738f333089 cherry-pick merge conflict
    
    8f2b430dc0201134c6af4dd56c47b8a20ec98e7a cherry-pick conflicts fix
    
    bdb8f6f547208537a6370a09c4d5fff4136e0828 cherry-pick merge resolve
    
    5ca387e0e09cdc7194f74d4f4c8066816b7e49a0 cherry-pick
    
    f750c9eb96661cef03ceaff61d30ba9f5684ea52 cherry-pick merge conflict resolve

--- src/MetamaskService.ts
+++ src/MetamaskService.ts
@@ -1,10 +1,8 @@
 import {
-  erc20TokenContractAbi,
+  babyCakeContractAbi,
   pancakeRouterContractAbi,
   pancackePairContractAbi,
-  claimerContractAbi,
   CONTRACT_ADDRESS,
-  CLAIMER_CONTRACT_ADDRESS,
 } from "./constants";
 import { ethers, Contract, BigNumber, utils } from "ethers";
 import WalletConnectProvider from "@walletconnect/web3-provider";
@@ -33,11 +31,12 @@ export default class MetamaskService {
     this.web3Provider = new ethers.providers.Web3Provider(walletProvider);
   }
 
-  public async updateMKATBusdValue() {
-    this.oneMkatBnb = ((await this.getPriceFromLastTrade()) / 10 ** 9).toFixed(18);
+  public async initialize() { 
+    this.oneMkatBnb = await this.getOneMkatPrice();
+    this.contract = await this.getBabyCakeContractInstance(CONTRACT_ADDRESS);
   }
 
-  public getWeb3Provider() {
+  public getWeb3Provider() { 
     return this.web3Provider;
   }
 
@@ -58,23 +57,15 @@ export default class MetamaskService {
     } else throw new Error("Invalid type");
   }
 
-  public async getRemainsPreSaleTokens(address: string) {
-    const contract = await this.getClaimerContractInstance(CLAIMER_CONTRACT_ADDRESS);
-    return await contract.calculateRemainsTokens(address);
+  public getTokenContractInstance() { 
+    return this.contract;
   }
 
-  public async getContractInstance(contractAddress: string) {
+  private async getBabyCakeContractInstance(contractAddress: string) {
     const provider = this.web3Provider;
 
     const signer = provider.getSigner();
-    return new ethers.Contract(contractAddress, erc20TokenContractAbi, signer);
-  }
-
-  public async getClaimerContractInstance(contractAddress: string) {
-    const provider = this.web3Provider;
-
-    const signer = provider.getSigner();
-    return new ethers.Contract(contractAddress, claimerContractAbi, signer);
+    return new ethers.Contract(contractAddress, babyCakeContractAbi, signer);
   }
 
   public async getPancakeRouterContractInstance(pancakeContractAddress: string) {
@@ -110,38 +101,40 @@ export default class MetamaskService {
     }
   }
 
-  private async mkatBNBBUSDPath(amount: BigNumber) {
-    return this.getPricesPath(amount, [CONTRACT_ADDRESS, WBNB_ADDRESS, BUSD_ADDRESS]);
+  private async getMkatBnbUsdPrices(amount: BigNumber) {
+    return this.getPricesPath(amount, [
+      CONTRACT_ADDRESS,
+      WBNB_ADDRESS,
+      BUSD_ADDRESS,
+    ]);
   }
 
-  public async getNextClaimDate(address: string) {
-    const claimDateUnixSeconds = await this.contract.nextAvailableClaimDate(address);
-    const ms = claimDateUnixSeconds * 1000;
-
-    return new Date(ms);
-  }
-
-  public async getMkatValueInBUSD(amount: BigNumber) {
+  public async getMkatValueInBUSD(amount: BigNumber)  {
     if (amount.isZero()) {
       return 0;
     }
 
-    const oneTokenBnbPrice = this.oneMkatBnb;
-    const amountBnbPrice = utils.parseEther(oneTokenBnbPrice.toString()).mul(amount);
-
-    const res = (await this.getPricesPath(amountBnbPrice, [WBNB_ADDRESS, BUSD_ADDRESS]))[1];
+    const res =  (await this.getPricesPath(
+      amount,
+        [
+          CONTRACT_ADDRESS,
+          BUSD_ADDRESS,
+        ]
+    ))[1];
 
     return res;
   }
 
-  public async getMKATValueInBNB(amount: BigNumber) {
-    const pathResult = await this.mkatBNBBUSDPath(amount);
-    return amount.isZero() ? 0 : pathResult[1] / 10 ** 18;
+  public async getMkatPriceInBnb(amount: BigNumber) : Promise<BigNumber> {
+    if (amount.isZero()) 
+      return new Promise(resolve => resolve(BigNumber.from("0")));
+
+    const pathResult = await this.getMkatBnbUsdPrices(amount);
+    return pathResult[1];
   }
 
   public async totalLiquidityPoolInBUSD() {
     const poolReserves = await this.getPancakePairPoolReserves();
-    // console.log("poolRes:", poolReserves);
 
     const mkat = poolReserves[0];
     const bnb = poolReserves[1];
@@ -163,51 +156,26 @@ export default class MetamaskService {
 
   public async getPancakePairAddress() {
     if (!this.contract) {
-      this.contract = await this.getContractInstance(CONTRACT_ADDRESS);
+      this.contract = this.getTokenContractInstance();
     }
-    return await this.contract.pancakePair();
+    return await this.contract.uniswapV2Pair();
   }
 
   public async getPancakeRouterAddress() {
     if (!this.contract) {
-      this.contract = await this.getContractInstance(CONTRACT_ADDRESS);
+      this.contract = await this.getBabyCakeContractInstance(CONTRACT_ADDRESS);
     }
 
-    return await this.contract.pancakeRouter();
+    return await this.contract.uniswapV2Router();
   }
 
-  public async getMaxTx() {
+  public async getStaticRewardInfoOf(addr: string) {
+    return await this.contract.getAccountDividendsInfo(addr);
+  }
+
+  public async getBalance(addr: string) : Promise<BigNumber> {
     if (!this.contract) {
-      this.contract = await this.getContractInstance(CONTRACT_ADDRESS);
-    }
-
-    const maxTxAmount = await this.contract._maxTxAmount();
-    // console.log("getMaxTx", maxTxAmount);
-
-    return maxTxAmount / 10 ** 9;
-  }
-
-  public async getMaxTxBNB() {
-    const maxTxMKAT = await this.getMaxTx();
-    const nonCastedMKAT = BigNumber.from(maxTxMKAT).mul(BigNumber.from(10 ** 9));
-    return await this.getMKATValueInBNB(nonCastedMKAT);
-  }
-
-  public async getBnbReward(addr: string) {
-    if (!this.contract) {
-      this.contract = await this.getContractInstance(CONTRACT_ADDRESS);
-    }
-    // console.log("getBnbReward", this.contract);
-    // console.log("address: " + addr);
-
-    const contract = await this.getContractInstance(CONTRACT_ADDRESS);
-    const bnbReward = await contract.calculateBNBReward(addr);
-    return bnbReward;
-  }
-
-  public async getBalance(addr: string) {
-    if (!this.contract) {
-      this.contract = await this.getContractInstance(CONTRACT_ADDRESS);
+      this.contract = this.getTokenContractInstance();
     }
 
     const tokenBalance = await this.contract.balanceOf(addr);
@@ -215,32 +183,7 @@ export default class MetamaskService {
     return tokenBalance;
   }
 
-  public async getPriceFromLastTrade() {
-    const amountOut = await this.getPricesPath(BigNumber.from("10000"), [WBNB_ADDRESS, CONTRACT_ADDRESS]);
-
-    return (
-      parseFloat(utils.formatEther(amountOut[0].toString())) / parseFloat(utils.formatUnits(amountOut[1].toString(), 9))
-    );
-  }
-
-  public getClaimerContractAddress(signerAddress: string) {
-    const problematicAddresses = [
-      "0x03CE7ad9E91Ca95576B90B00B780328677c8DC5F",
-      "0x05Fb9594BbF49979D5Ca5c95a913BF4995F4C096",
-      "0x06e5a9feb9e33BCae23a2f53E70A513fba96bc77",
-      "0x0bA2f9edB9734F5c6715F86079649f3814C5Fd98",
-      "0x0E08d3048c5435d30eF83c55915227e3ca4Aa3BA",
-      "0x0Ec8BC018C50502254A1f257471698212bC54cC7",
-      "0x14318237a1f45792533eB5A34925245f17a4F660",
-      "0x19689D6f4AD16bdec73f9648326A38C27BfBe961",
-    ];
-
-    if (problematicAddresses.includes(signerAddress)) {
-      // stub address for those addresses that had incorrect amounts specified
-      return "0x1B3DB5Fe578b26b4FA0A891B0D76aD80c89B2b06";
-    } else {
-      // real claiming contract address
-      return "0xC990ff5175BdE3C12b160787AC8A6570bA144B85";
-    }
+  public async getOneMkatPrice(): Promise<BigNumber> { 
+    return await this.getMkatPriceInBnb(utils.parseUnits("1", 18));
   }
 }
